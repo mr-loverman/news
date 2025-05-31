e0f4bde56f272683b8e7ca1175afbd37259172e9
Jj <jjteodoro5231@gmail.com>
2025-05-31 23:06:30 +0800

    Finished Home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import Card from '../Components/Card'
 import styles from '../Styles/Home.module.css'
 import placeholder from '../assets/Placeholder.jpg'
 
@@ -5,14 +6,34 @@ function Home() {
 
   return(
     <>
-      <div>
-        <div className={styles.Card_container_home} >
+      <div className={styles.Home_container} >
+        <div className={styles.Header_container_home} >
           <div className={styles.Home_header_top_side}>
-            <img src={placeholder} />
+            <img 
+            src={placeholder} 
+            className={styles.Image} 
+            />
           </div>
-          <div className={styles.Home_header_top_side}></div>
+          <div className={styles.Home_header_bottom_side}>
+            <div>
+              <h1 className={styles.Header_title} >
+                Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit
+              </h1>
+            </div>
+            <div>
+              <h5 className={styles.Date} >
+                May 31, 2025
+              </h5>
+            </div>
+          </div>
         </div>
-        <div></div>
+        <div className={styles.Separator} ></div>
+        <div className={styles.Home_bottom_side} >
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
       </div>
     </>
   )
